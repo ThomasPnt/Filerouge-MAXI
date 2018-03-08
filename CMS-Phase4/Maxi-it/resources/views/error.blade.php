87a7bfca6a379f8b8f02db4ee60d276217f37652
Harimaron <gaetan.ribonnet@outlook.com>
2018-03-08 11:22:58 +0100

    Footer and error.

---
+++ CMS-Phase4/Maxi-it/resources/views/error.blade.php
@@ -0,0 +1,31 @@
+<div>
+  <h1>This page doesn't exist!</h1>
+</div>
+<style>
+        @import url(http://fonts.googleapis.com/css?family=Montserrat:400,700); 
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  background: #303f42;
+  font-family: Montserrat, sans-serif;
+  color: #ebb256;
+}
+
+div {
+  width: 100%;
+}
+
+
+h1 {
+  text-align: center;
+  font-size: 5vh;
+  color: #ebb256;
+  text-shadow: 0 0 1    px #000;
+}
+</style>
